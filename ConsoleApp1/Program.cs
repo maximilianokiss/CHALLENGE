e2709302b2573f8ffe1070deb087fb577d7138d4
Maximiliano Kiss <104541666+maximilianokiss@users.noreply.github.com>
2024-01-09 14:21:45 -0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+var sopa = new List<string>();
+int longitud = 64;
+
+for (int i = 0; i < 64; i++)
+{
+    string palabra = WordFinder.GenerarStringAleatorio(longitud);
+    sopa.Add(palabra);
+}
+
+sopa[3] = "coldxiidkzmwenagcoxrtwkmrnuomikoddxcfnnhkikruvpmeijndywmtcmnqwkf";
+sopa[4] = "cdldxiidkzmwenagcoxrtwkmrnuomikoddxcfnnhkikruvpmeijndywmtcmndniw";
+
+
+IEnumerable<string> wordstream = new List<string>
+{
+    "cold",
+    "wind",
+    "snow",
+    "chill",
+};
+
+WordFinder wF = new WordFinder(sopa);
+var resultado = wF.Find(wordstream);
+if (resultado != null)
+{
+    Console.WriteLine("Palabras encontradas: \n");
+    foreach (var item in resultado)
+    {
+        Console.WriteLine(item);
+    }
+}
---
+++ ConsoleApp1/WordFinder.cs
@@ -0,0 +1,88 @@
+﻿using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class WordFinder
+    {
+        private IEnumerable<string> _matrix;
+        public WordFinder(IEnumerable<string> matrix)
+        {
+            this._matrix = matrix;
+
+            foreach (var item in matrix)
+                Console.WriteLine(item);
+        }
+        public IEnumerable<string> Find(IEnumerable<string> wordstream)
+        {
+            var frecPalabras = new Dictionary<string, int>();
+
+            foreach (var word in wordstream)
+            {
+                var esta = BuscaPalabra(word);
+                if (esta)
+                {
+                    // Cuenta cada palabra una vez sola
+                    if (!frecPalabras.ContainsKey(word))
+                        frecPalabras.Add(word, 1);
+                }
+            }
+
+            // top 10 mas repetidas
+            var topWords = frecPalabras.OrderByDescending(kv => kv.Value).Take(10).Select(kv => kv.Key);
+
+            return topWords;
+        }
+        private bool BuscaPalabra(string word)
+        {
+            //busca horizontalmente de derecha a izquierda
+            foreach (string d in this._matrix)
+            {
+                if (d.ToLower().Contains(word.ToLower()))
+                    return true;
+            }
+
+            //busca horizontalmente de izquierda a derechad
+            foreach (string d in this._matrix)
+            {
+                if (reverse(d).Contains(word.ToLower()))
+                    return true;
+            }
+
+            //busca verticalmente
+            StringBuilder caracteresVerticales = new StringBuilder();
+            for (int col = 0; col < 4; col++)
+            {
+                foreach (string d in this._matrix)
+                    caracteresVerticales.Append(d.Substring(col, 1));
+
+                if (caracteresVerticales.ToString().ToLower().Contains(word.ToLower()))
+                    return true;
+
+                if (reverse(caracteresVerticales.ToString()).ToLower().Contains(word.ToLower()))
+                    return true;
+            }
+
+            return false;
+        }
+        public static string GenerarStringAleatorio(int longitud)
+        {
+            Random random = new Random();
+            const string alfabeto = "abcdefghijklmnopqrstuvwxyz";
+
+            char[] caracteresAleatorios = new char[longitud];
+
+            for (int i = 0; i < longitud; i++)
+            {
+                int indice = random.Next(alfabeto.Length);
+                caracteresAleatorios[i] = alfabeto[indice];
+            }
+
+            return new string(caracteresAleatorios);
+        }
+        public string reverse(string str)
+        {
+            return new string(str.Reverse().ToArray());
+        }
+    }
+}
+
